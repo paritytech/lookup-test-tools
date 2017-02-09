a49845f35e6f18ea3c21f0cbdf84a32976be5219
Jannis R <mail@jannisr.de>
2017-02-09 19:11:35 +0100

    BadgeReg & TokenReg fixtures

--- fetch-contracts.sh
+++ fetch-contracts.sh
@@ -3,3 +3,5 @@
 curl --fail -L 'https://raw.githubusercontent.com/ethcore/contracts/master/SimpleRegistry.sol' > contracts/SimpleRegistry.sol
 curl --fail -L 'https://raw.githubusercontent.com/ethcore/contracts/master/ProofOfEmail.sol' > contracts/ProofOfEmail.sol
 curl --fail -L 'https://raw.githubusercontent.com/ethcore/contracts/master/SMSVerification.sol' > contracts/SMSVerification.sol
+curl --fail -L 'https://raw.githubusercontent.com/ethcore/contracts/master/BadgeReg.sol' > contracts/BadgeReg.sol
+curl --fail -L 'https://raw.githubusercontent.com/ethcore/contracts/master/TokenReg.sol' > contracts/TokenReg.sol
